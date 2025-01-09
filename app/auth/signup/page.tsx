7916848b4662830962c89c0583a100a6152f4e26
Aliyu Bello <aliyubababello@gmail.com>
2025-01-09 17:16:35 +0100

    check subdomain added

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -19,6 +19,9 @@ import { handleSignUpSubmit } from '../utils/handleSignUpSubmit';
 import styles from "../styles/auth.module.css";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'
+import axios from 'axios';
+import { SERVER_URL } from '../../../constants/url';
+import { useDebouncedCallback } from 'use-debounce';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -30,132 +33,170 @@ export default function SignUp() {
     const [subdomain, setSubDomain] = React.useState('');
     const [name, setName] = React.useState('');
     const router = useRouter();
+    const [isAvailable, setIsAvailable] = React.useState<boolean>();
 
     const handleSubmit = async (event: any) => {
-        setLoading('Sending data..');
-        handleSignUpSubmit(event, setError, setSuccess, setLoading, router)
+        if (isAvailable) {
+            setLoading('Sending data..');
+            handleSignUpSubmit(event, setError, setSuccess, setLoading, router, subdomain)
+        } else {
+            setIsAvailable(false);
+        }
     }
 
-    const handleSetNameAndSubDomain = (event: any) => {
+
+    const isSubdomainAvailable = async (subdomain: string) => {
+        const { data } = await axios.get(`${SERVER_URL}/auth?subdomain=${subdomain}`);
+        if (data.status === "success" && data.data.result) {
+            setIsAvailable(true);
+            // return true;
+        } else {
+            setIsAvailable(false);
+            // return false;
+        }
+    }
+
+    const handleCheckSubdomain = useDebouncedCallback(async (subdomain: string) => {
+        await isSubdomainAvailable(subdomain)
+    }, 500);
+
+    const handleSetNameAndSubdomain = async (event: any) => {
         const { name, value } = event.target;
         setName(value)
         setSubDomain(value.trim().toLowerCase().split(' ').join(''));
+        await handleCheckSubdomain(subdomain);
     }
 
+    const handleSetSubdomain = async (event: any) => {
+        const { name, value } = event.target;
+        setSubDomain(value.trim().toLowerCase().split(' ').join(''));
+        await handleCheckSubdomain(subdomain);
+    }
+
+
+
     return (
-            <ThemeProvider theme={defaultTheme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
                     <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate={false}
+                        sx={{ mt: 3 }}
                     >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign up
-                        </Typography>
-                        <Box
-                            component="form"
-                            onSubmit={handleSubmit}
-                            noValidate={false}
-                            sx={{ mt: 3 }}
-                        >
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        autoComplete="given-name"
-                                        name="first_name"
-                                        fullWidth
-                                        id="first_name"
-                                        label="Business Name"
-                                        type='text'
-                                        autoFocus
-                                        onChange={handleSetNameAndSubDomain}
-                                        value={name}
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="first_name"
+                                    fullWidth
+                                    id="first_name"
+                                    label="Business Name"
+                                    type='text'
+                                    autoFocus
+                                    onChange={handleSetNameAndSubdomain}
+                                    value={name}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Grid item xs={6} sm={6}>
                                     <TextField
                                         required
                                         fullWidth
+                                        size='small'
                                         id="last_name"
                                         label="Last Name"
                                         name="Subdomain"
                                         type='text'
                                         autoComplete="family-name"
                                         value={subdomain}
-                                    />
-                                    <TextField defaultValue={'.siniotech.com.ng'} disabled />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="email"
-                                        label="Email Address"
-                                        type='email'
-                                        name="email"
-                                        autoComplete="email"
+                                        onChange={handleSetSubdomain}
                                     />
                                 </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="password"
-                                    />
+                                <Grid item xs={6} sm={6}>
+                                    <TextField size='small' defaultValue={'.siniotech.com.ng'} disabled />
                                 </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        name="confirm_password"
-                                        label="Confirm Password"
-                                        type="password"
-                                        id="confirm_password"
-                                        autoComplete="password"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    {error && (<Box className={styles.formError} textAlign={'center'}>{error.toLowerCase()}</Box>)}
-                                    {success && (<Box className={styles.formSuccess} textAlign={'center'}>{success.toLowerCase()}</Box>)}
-                                    {loading && <Box textAlign={"center"} sx={{ color: "green" }}>{loading.toUpperCase()}</Box>}
-                                    <FormControlLabel
-                                        control={<Checkbox value="allowExtraEmails" color="primary" name='remember_me' />}
-                                        label="I want to receive inspiration, marketing promotions and updates via email."
-                                    />
-                                </Grid>
+
+                                {isAvailable?<Box sx={{color:'green'}}>It is available</Box>:<Box sx={{color:'red'}}>It is not available</Box>}
                             </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                                color='success'
-                            >
-                                Sign Up
-                            </Button>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="/auth/signin" variant="body2">
-                                        Already have an account? Sign in
-                                    </Link>
-                                </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    type='email'
+                                    name="email"
+                                    autoComplete="email"
+                                />
                             </Grid>
-                        </Box>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="confirm_password"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="confirm_password"
+                                    autoComplete="password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                {error && (<Box className={styles.formError} textAlign={'center'}>{error.toLowerCase()}</Box>)}
+                                {success && (<Box className={styles.formSuccess} textAlign={'center'}>{success.toLowerCase()}</Box>)}
+                                {loading && <Box textAlign={"center"} sx={{ color: "green" }}>{loading.toUpperCase()}</Box>}
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" name='remember_me' />}
+                                    label="I want to receive inspiration, marketing promotions and updates via email."
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            color='success'
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/auth/signin" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
                     </Box>
-                    <Copyright sx={{ mt: 5 }} />
-                </Container>
-            </ThemeProvider>
-        );
-    }
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider >
+    );
+}
